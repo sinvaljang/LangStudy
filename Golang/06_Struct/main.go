52c4c68742a396da7b59b6270a7aad607b8c57fa
sinvaljang <sinvaljang@gmail.com>
2018-03-25 17:23:28 +0900

    study

---
+++ Golang/06_Struct/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Coner",
+		contactInfo: contactInfo{
+			email:   "jim@go.com",
+			zipCode: 111111,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
